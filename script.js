8afc887fd1b1eba22954cb26735e40d4e1e233fb
izabelatyma <61478452+izabelatyma@users.noreply.github.com>
2020-05-07 14:10:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function gameOver(gameWon) {
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].removeEventListener('click', turnClick, false);
 	}
-	declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
+	declareWinner(gameWon.player == huPlayer ? "Uavgjort" : "Du tapte :(");
 }
 
 function declareWinner(who) {
@@ -82,7 +82,7 @@ function checkTie() {
 			cells[i].style.backgroundColor = "green";
 			cells[i].removeEventListener('click', turnClick, false);
 		}
-		declareWinner("Tie Game!")
+		declareWinner("Uavgjort")
 		return true;
 	}
 	return false;
